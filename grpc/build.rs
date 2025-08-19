f2c3cedfeeec7abe4a30561cb51f9f5468ce3396
Aleksandr Provotorov <provotorov@inbox.ru>
2025-08-19 11:53:12 +0300

    some protobuf changes

--- grpc/build.rs
+++ grpc/build.rs
@@ -4,6 +4,9 @@ fn main() {
     let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
     tonic_prost_build::configure()
         .file_descriptor_set_path(out_dir.join("smm_descriptor.bin"))
-        .compile_protos(&["proto/smm/v1/smm.proto"], &["proto/smm"])
+        .compile_protos(
+            &["proto/users/v1/users.proto"],
+            &["proto/users", "proto/vendor"],
+        )
         .unwrap();
 }
--- grpc/src/lib.rs
+++ grpc/src/lib.rs
@@ -1,4 +1,6 @@
 pub mod smm {
-    tonic::include_proto!("proto.smm.v1");
+    pub mod users {
+        tonic::include_proto!("proto.users.v1");
+    }
     pub const FILE_DESCRIPTOR_SET: &[u8] = tonic::include_file_descriptor_set!("smm_descriptor");
 }
